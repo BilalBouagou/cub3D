63b1132c105d12317d92a9cc2aa40cc87c6d8224
youssef el hadraoui <93286066+yel-hadr@users.noreply.github.com>
2023-10-25 04:07:27 +0100

    day01 for staring to be a fugging gameDev

--- src/rendering/renderer.c
+++ src/rendering/renderer.c
@@ -3,11 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   renderer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yel-hadr < yel-hadr@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:26:05 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/10/23 00:37:58 by bbouagou         ###   ########.fr       */
+/*   Updated: 2023/10/25 03:44:23 by yel-hadr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/renderer.h"
+
+void	ft_rending(t_data *data)
+{
+	data->mlx = mlx_init(WINDOW_WIDTH, WINDOW_HIGHT, "Cub3D", false);
+	if (!data->mlx)
+	{
+		ft_putstr_fd((char *)mlx_strerror(mlx_errno), 2);
+		exit (EXIT_FAILURE);
+	}
+	data->img = mlx_new_image(data->mlx, WINDOW_WIDTH, WINDOW_HIGHT);
+	if (!data->img)
+	{
+		mlx_close_window(data->mlx);
+		ft_putstr_fd((char *)mlx_strerror(mlx_errno), 2);
+		exit (EXIT_FAILURE);
+	}
+}

62aa305befb92f768f3a7d0844ecdce05b75db31
BilalBouagou <bilalbouagou@protonmail.com>
2023-12-01 21:49:38 +0100

    fixed error messages

--- cub3d_bonus/inc/parser.h
+++ cub3d_bonus/inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 04:04:27 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/11/29 19:08:47 by bbouagou         ###   ########.fr       */
+/*   Updated: 2023/12/01 21:48:52 by bbouagou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 # include "common.h"
 
-# define FILEOPENERR "Can't open the map file\n"
-# define ARGSERR "Something's wrong with the given arguments.\n"
-# define MAPSIZERR "The size of the map doesn't seem right\n"
-# define FILEFRMTERR "There's something wrong with the file format\n"
-# define NOTXTERR "Can't open NO texture file\n"
-# define SOTXTERR "Can't open SO texture file\n"
-# define WETXTERR "Can't open WE texture file\n"
-# define EATXTERR "Can't open EA texture file\n"
-# define PLYRPOSERR "Multiple player starting positions detected\n"
-# define NOPLYPOSERR "No player starting position has been detected\n"
-# define MAPSTRTERR "The map seems to be not enclosed by walls\n"
-# define GUNERR "Can't open gun texture file\n"
+# define FILEOPENERR "Error\nCan't open the map file\n"
+# define ARGSERR "Error\nSomething's wrong with the given arguments.\n"
+# define MAPSIZERR "Error\nThe size of the map doesn't seem right\n"
+# define FILEFRMTERR "Error\nThere's something wrong with the file format\n"
+# define NOTXTERR "Error\nCan't open NO texture file\n"
+# define SOTXTERR "Error\nCan't open SO texture file\n"
+# define WETXTERR "Error\nCan't open WE texture file\n"
+# define EATXTERR "Error\nCan't open EA texture file\n"
+# define PLYRPOSERR "Error\nMultiple player starting positions detected\n"
+# define NOPLYPOSERR "Error\nNo player starting position has been detected\n"
+# define MAPSTRTERR "Error\nThe map seems to be not enclosed by walls\n"
+# define GUNERR "Error\nCan't open gun texture file\n"
 
 void	parser(int fd, t_data *data, int map_len);
 
